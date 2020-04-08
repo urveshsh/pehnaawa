97478eff0bb9b0b7353cd1019b6621db81144a8e
urveshsh <urveshsh@gmail.com>
2020-04-08 17:22:43 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import Header from './components/hearder/header';
+
 import './App.css';
-import HomePage from './homepage';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/directory';
+import './homepage.scss'
+
+export const HomePage = () => {
+    return(
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage;
